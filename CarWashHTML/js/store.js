0160ce8797ae9f8e54ebbb096d00d4b12c44410a
Barnett Lim <barnett.lim@belldirect.com.au>
2026-04-27 12:12:37 +1000

    Add project files.

---
+++ CarWashHTML/js/store.js
@@ -0,0 +1,28 @@
+// Demo in-memory store
+window.Store = {
+  jobs: [
+    {
+      id: "JOB-001",
+      plate: "SGX1234A",
+      customerType: "Guest",
+      status: "CREATED"
+    }
+  ],
+
+  createJob(plate, customerType) {
+    this.jobs.push({
+      id: "JOB-" + Date.now(),
+      plate,
+      customerType,
+      status: "CREATED"
+    });
+  },
+
+  getAvailableJobs() {
+    return this.jobs.filter(j => j.status === "CREATED");
+  },
+
+  getJob(id) {
+    return this.jobs.find(j => j.id === id);
+  }
+};
